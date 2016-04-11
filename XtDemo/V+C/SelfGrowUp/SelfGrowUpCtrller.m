9fc660c8a3b03d6129b55dd5dd8285b926ce8ee1
teason <teason@appletekimbp.lan>
2016-04-11 16:07:59 +0800

    update zoom animate

--- XtDemo/V+C/SelfGrowUp/SelfGrowUpCtrller.m
+++ XtDemo/V+C/SelfGrowUp/SelfGrowUpCtrller.m
@@ -11,10 +11,14 @@
 #import "YYModel.h"
 #import "SelfChartView.h"
 #import "RootCtrl+LoadingLauncherScene.h"
+#import "MMPulseView.h"
 
 
 @interface SelfGrowUpCtrller () <UITextFieldDelegate>
-
+{
+    MMPulseView *bt1_pulseView ;
+    MMPulseView *bt2_pulseView ;
+}
 @property (nonatomic,strong) NSMutableArray *list_7days ;
 @property (nonatomic,strong) NSMutableArray *list_30days ;
 
@@ -145,6 +149,9 @@
     self.textfield.delegate = self ;
     self.tabBarController.tabBar.tintColor = [UIColor darkGrayColor] ;
 
+    _btYue.layer.cornerRadius = _btYue.frame.size.width / 2. ;
+    _btZhou.layer.cornerRadius = _btZhou.frame.size.width / 2. ;
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -152,7 +159,68 @@
     [super viewWillAppear:animated] ;
     
     [self modalIntoLauncher] ;
+    
+    [self pulseView] ;
 }
+
+
+- (void)viewDidLayoutSubviews
+{
+    [self pulseView] ;
+}
+
+
+- (void)pulseView
+{
+//    
+    if (bt1_pulseView || bt2_pulseView) {
+        [bt1_pulseView stopAnimation] ;
+        [bt1_pulseView removeFromSuperview] ;
+        bt1_pulseView = nil ;
+        
+        [bt2_pulseView stopAnimation] ;
+        [bt2_pulseView removeFromSuperview] ;
+        bt2_pulseView = nil ;
+    }
+    
+    if (!bt1_pulseView || !bt2_pulseView)
+    {
+        bt1_pulseView = [MMPulseView new] ;
+        bt1_pulseView.frame = CGRectMake(0,0,200,200);
+        [self.view addSubview:bt1_pulseView];
+        
+        bt2_pulseView = [MMPulseView new] ;
+        bt2_pulseView.frame = CGRectMake(0,0,200,200);
+        [self.view addSubview:bt2_pulseView];
+    }
+    
+    bt1_pulseView.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    bt1_pulseView.colors = @[(__bridge id)[UIColor xt_lightGreenColor].CGColor,
+                             (__bridge id)[UIColor xt_mainColor].CGColor,
+                             (__bridge id)[UIColor xt_lightGreenColor].CGColor];
+    bt1_pulseView.minRadius = 20;
+    bt1_pulseView.maxRadius = 50;
+    bt1_pulseView.duration = 5;
+    bt1_pulseView.count = 6;
+    bt1_pulseView.lineWidth = 1.0f;
+    bt1_pulseView.center = self.btZhou.center ;
+    [bt1_pulseView startAnimation];
+    [self.view bringSubviewToFront:self.btZhou] ;
+//
+    bt2_pulseView.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    bt2_pulseView.colors = @[(__bridge id)[UIColor whiteColor].CGColor,
+                             (__bridge id)[UIColor xt_mainColor].CGColor,
+                             (__bridge id)[UIColor whiteColor].CGColor];
+    bt2_pulseView.minRadius = 20;
+    bt2_pulseView.maxRadius = 50;
+    bt2_pulseView.duration = 3;
+    bt2_pulseView.count = 1;
+    bt2_pulseView.lineWidth = 9.0f;
+    bt2_pulseView.center = self.btYue.center ;
+    [bt2_pulseView startAnimation];
+    [self.view bringSubviewToFront:self.btYue] ;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
